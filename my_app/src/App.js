a7c13adfcffa7e78c9e839fc2174d34273db8ae8
Girija Vaidya <vaidyagirija58@gmail.com>
2026-01-02 14:35:03 +0530

    Three-box can add into UI

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,31 +1,46 @@
-
 import './App.css';
-
 import { MdSunny } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
-      <>
-      <div className='search-wrapper'>
-      <input className='search-box'  type='text' placeholder='Enter the City' />
-      
-</div>
-      <div className='weather-container'>
-        <div className='current-weather-location'>
-         <MdSunny className='sun-icon' />
-         <div>
-          <h3>Today</h3>
-          <h1>India</h1>
-          <h3>Temperature 17°C</h3>
 
-         </div>
+      {/* Search Box */}
+      <div className="search-wrapper">
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Enter the City"
+        />
+      </div>
+
+      {/* Weather Section */}
+      <div className="weather-container">
+        <div className="container">
+
+          <div className="row">
+            <div className="col-md-4">
+              <div className="current-weather-location">
+                <MdSunny className="sun-icon" />
+                <div>
+                  <h3>Today</h3>
+                  <h1>India</h1>
+                  <h3>Temperature 17°C</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* 3 Boxes */}
+          <div className="three-block">
+            <div className="box">Box 1</div>
+            <div className="box">Box 2</div>
+            <div className="box">Box 3</div>
+          </div>
 
         </div>
-
       </div>
-     
-      </>
+
     </div>
   );
 }
