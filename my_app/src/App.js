7c19e79cde08ad7e6cf9a4e9eb6a8c194a34f3ee
Girija Vaidya <vaidyagirija58@gmail.com>
2026-01-03 19:34:11 +0530

    Changes the ui in box

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { MdSunny } from "react-icons/md";
-
+import { IoMdSunny } from "react-icons/io";
+import { FaCloudBolt } from "react-icons/fa6";
 function App() {
   return (
     <div className="App">
@@ -19,13 +22,14 @@ function App() {
         <div className="container">
 
           <div className="row">
-            <div className="col-md-4">
+            {/* <div className="col-md-6"> */}
               <div className="current-weather-location">
                 <MdSunny className="sun-icon" />
                 <div>
-                  <h3>Today</h3>
-                  <h1>India</h1>
-                  <h3>Temperature 17°C</h3>
+                  <h6>Today</h6>
+                  <h1 className='fw-bold fs-1'>India</h1>
+                  <h7 className='fw-semibold'>Temperature 17°C</h7>
+                 
                 </div>
               </div>
             </div>
@@ -33,15 +37,41 @@ function App() {
 
           {/* 3 Boxes */}
           <div className="three-block">
-            <div className="box">Box 1</div>
-            <div className="box">Box 2</div>
-            <div className="box">Box 3</div>
+            <div className="box">
+              <div className='weather-check'>
+              <h5 className='fw-bold text-white'>Wednesday</h5>
+              <IoMdSunny className='text-grey fs-1'  />
+              <h5 className='text-white'>21°C</h5>
+              </div>
+            </div>
+            <div className="box">
+              <div className='weather-check'>
+              <h5 className='fw-bold text-white'>Thrusday</h5>
+              <FaCloudBolt className='text-grey fs-1'  />
+                <h5 className='text-white'>21°C</h5>
+              </div>
+            </div>
+            <div className="box">
+              <div className='weather-check'>
+
+              <h5 className='fw-bold text-white'>Friday</h5>
+               <IoMdSunny className='text-grey fs-1'  />
+                 <h5 className='text-white'>21°C</h5>
+               </div>
+            </div>
+            <div className="box">
+              <div className='weather-check'>
+              <h5 className='fw-bold text-white'>Saturday</h5>
+              <FaCloudBolt className='text-grey fs-1'  />
+                <h5 className='text-white'>21°C</h5>
+              </div>
+            </div>
           </div>
 
         </div>
       </div>
 
-    </div>
+    // </div>
   );
 }
 
